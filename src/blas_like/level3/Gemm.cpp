7c09a5c1acfd61bb787fa408fbaa3919b819417e
Tom Benson <30674819+benson31@users.noreply.github.com>
2020-02-28 12:51:05 -0800

    Update src/blas_like/level3/Gemm.cpp
    Co-Authored-By: Tim Moon <moon13@llnl.gov>

--- src/blas_like/level3/Gemm.cpp
+++ src/blas_like/level3/Gemm.cpp
@@ -301,7 +301,7 @@ void LocalGemm
   T beta,        AbstractDistMatrix<T>& C)
 {
     EL_DEBUG_CSE;
-#ifdef EL_RELEASE
+#ifndef EL_RELEASE
     if(orientA == NORMAL && orientB == NORMAL)
     {
         if(A.ColDist() != C.ColDist() ||
