2eb71fe23511a02dc93cbbb7a270c0e5ec86a9b0
Tom Benson <benson31@llnl.gov>
2023-10-04 07:52:39 -0700

    Debugging code to assert proper device-ness of pointers in memcpy

--- include/hydrogen/device/gpu/rocm/ROCmCopy.hpp
+++ include/hydrogen/device/gpu/rocm/ROCmCopy.hpp
@@ -11,6 +11,19 @@
 
 #include <hip/hip_runtime.h>
 
+#include <stdio.h>
+
+#define TOM_PTR_OK(src, dst, kind)                                      \
+    do                                                                  \
+    {                                                                   \
+    if (!ok(src, dst, kind))                                            \
+    {                                                                   \
+        printf("%s:%d ERROR Bad ptrs in %s\n\n  src: %p (%s)\n\n  dst: %p (%s)\n\n", \
+               __FILE__, __LINE__, #kind, src, device_string(src), dst, device_string(dst)); \
+        throw std::string("BAD POINTERS");                              \
+    }                                                                   \
+        } while (0)
+
 namespace hydrogen
 {
 namespace gpu
@@ -34,6 +47,52 @@ void Fill1DBuffer(T* buffer, size_t num_elements, T value,
     Fill_GPU_1D_impl(buffer, num_elements, value, si);
 }
 
+inline hipMemoryType memtype(void const* ptr)
+{
+    hipPointerAttribute_t attr;
+    auto const status = hipPointerGetAttributes(&attr, ptr);
+    if (status == hipErrorInvalidValue)
+        return hipMemoryTypeHost;
+    H_CHECK_HIP(status);
+    return attr.memoryType;
+}
+
+inline bool is_host_ptr(void const* ptr)
+{
+    return memtype(ptr) == hipMemoryTypeHost;
+}
+
+inline bool is_device_ptr(void const* ptr)
+{
+    return memtype(ptr) == hipMemoryTypeDevice;
+}
+
+inline bool ok(void const* src, void const* tgt, hipMemcpyKind kind)
+{
+    switch (kind)
+    {
+    case hipMemcpyHostToDevice:
+        return is_host_ptr(src) && is_device_ptr(tgt);
+    case hipMemcpyHostToHost:
+        return is_host_ptr(src) && is_host_ptr(tgt);
+    case hipMemcpyDeviceToDevice:
+        return is_device_ptr(src) && is_device_ptr(tgt);
+    case hipMemcpyDeviceToHost:
+        return is_device_ptr(src) && is_host_ptr(tgt);
+    case hipMemcpyDefault:
+        return true;
+    default:
+        return false;
+    }
+}
+
+inline char const* device_string(void const* ptr)
+{
+    if (is_host_ptr(ptr)) return "host_ptr";
+    if (is_device_ptr(ptr)) return "device_ptr";
+    return "unknown_ptr";
+}
+
 template <typename T>
 void Copy1DIntraDevice(T const* H_RESTRICT src, T* H_RESTRICT dest,
                        size_t num_elements,
@@ -42,10 +101,11 @@ void Copy1DIntraDevice(T const* H_RESTRICT src, T* H_RESTRICT dest,
     if (num_elements == 0UL)
         return;
 
+    TOM_PTR_OK(src, dest, hipMemcpyDeviceToDevice);
     H_CHECK_HIP(
         hipMemcpyAsync(
             dest, src, num_elements*sizeof(T),
-            hipMemcpyDeviceToDevice, si.Stream()));
+            hipMemcpyDefault, si.Stream()));
 }
 
 template <typename T>
@@ -56,10 +116,11 @@ void Copy1DToHost(T const* H_RESTRICT src, T* H_RESTRICT dest,
     if (num_elements == 0UL)
         return;
 
+    TOM_PTR_OK(src, dest, hipMemcpyDeviceToHost);
     H_CHECK_HIP(
         hipMemcpyAsync(
             dest, src, num_elements*sizeof(T),
-            hipMemcpyDeviceToHost, src_si.Stream()));
+            hipMemcpyDefault, src_si.Stream()));
 }
 
 template <typename T>
@@ -70,10 +131,11 @@ void Copy1DToDevice(T const* H_RESTRICT src, T* H_RESTRICT dest,
     if (num_elements == 0UL)
         return;
 
+    TOM_PTR_OK(src, dest, hipMemcpyHostToDevice);
     H_CHECK_HIP(
         hipMemcpyAsync(
             dest, src, num_elements*sizeof(T),
-            hipMemcpyHostToDevice, dest_si.Stream()));
+            hipMemcpyDefault, dest_si.Stream()));
 }
 
 template <typename T>
@@ -85,12 +147,13 @@ void Copy2DIntraDevice(T const* src, size_t src_ldim,
     if (height == 0UL || width == 0UL)
         return;
 
+    TOM_PTR_OK(src, dest, hipMemcpyDeviceToDevice);
     H_CHECK_HIP(
         hipMemcpy2DAsync(
             dest, dest_ldim*sizeof(T),
             src, src_ldim*sizeof(T),
             height*sizeof(T), width,
-            hipMemcpyDeviceToDevice, si.Stream()));
+            hipMemcpyDefault, si.Stream()));
 }
 
 template <typename T>
@@ -102,12 +165,13 @@ void Copy2DToHost(T const* src, size_t src_ldim,
     if (height == 0UL || width == 0UL)
         return;
 
+    TOM_PTR_OK(src, dest, hipMemcpyDeviceToHost);
     H_CHECK_HIP(
         hipMemcpy2DAsync(
             dest, dest_ldim*sizeof(T),
             src, src_ldim*sizeof(T),
             height*sizeof(T), width,
-            hipMemcpyDeviceToHost, src_si.Stream()));
+            hipMemcpyDefault, src_si.Stream()));
 }
 
 template <typename T>
@@ -119,12 +183,13 @@ void Copy2DToDevice(T const* src, size_t src_ldim,
     if (height == 0UL || width == 0UL)
         return;
 
+    TOM_PTR_OK(src, dest, hipMemcpyHostToDevice);
     H_CHECK_HIP(
         hipMemcpy2DAsync(
             dest, dest_ldim*sizeof(T),
             src, src_ldim*sizeof(T),
             height*sizeof(T), width,
-            hipMemcpyHostToDevice, dest_si.Stream()));
+            hipMemcpyDefault, dest_si.Stream()));
 }
 
 }// namespace gpu
