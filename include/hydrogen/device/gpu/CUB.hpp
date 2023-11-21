587ffa4f61d0277b4795432aaa2680b36b9d0806
Tal Ben-Nun <tbennun@users.noreply.github.com>
2023-11-21 10:10:59 -0700

    Extended GPU memory pool (#172)
    * Port CUB binned memory allocator to Hydrogen
    
    * Add tests
    
    * Implement linear and custom binned allocation
    
    * Modify human-readable output
    
    * Implement mallocAsync backend
    
    * Improve reporting and configurability
    
    * Documentation update
    
    * Use streams when freeing
    
    * Revert "Use streams when freeing"
    
    This reverts commit 5fe11e55cb10ff0e233129f02299d0b30169f9fa.
    
    * Remove need for active stream in free
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Benson <benson31@llnl.gov>
    
    ---------
    
    Co-authored-by: Tom Benson <benson31@llnl.gov>

--- include/hydrogen/device/gpu/CUB.hpp
+++ include/hydrogen/device/gpu/CUB.hpp
@@ -2,23 +2,12 @@
 #define HYDROGEN_IMPORTS_CUB_HPP_
 
 #include "El/hydrogen_config.h"
-
-#ifdef HYDROGEN_HAVE_CUDA
-#include <cuda_runtime.h>
-#include <cub/util_allocator.cuh>
-#elif defined HYDROGEN_HAVE_ROCM
-#include <hipcub/hipcub.hpp>
-#endif // HYDROGEN_HAVE_CUB
+#include "hydrogen/PoolAllocator.hpp"
 
 namespace hydrogen
 {
 namespace cub
 {
-#ifdef HYDROGEN_HAVE_CUDA
-namespace cub_impl = ::cub;
-#elif defined HYDROGEN_HAVE_ROCM
-namespace cub_impl = ::hipcub;
-#endif // HYDROGEN_HAVE_CUDA
 
     /** @brief Get singleton instance of CUB memory pool.
      *
@@ -26,19 +15,30 @@ namespace cub_impl = ::hipcub;
      *  already. The following environment variables are used to
      *  control the construction of the memory pool:
      *
-     *    - H_CUB_BIN_GROWTH: The growth factor. (Default: 2)
-     *    - H_CUB_MIN_BIN: The minimum bin. (Default: 1)
-     *    - H_CUB_MAX_BIN: The maximum bin. (Default: no max bin)
-     *    - H_CUB_MAX_CACHED_SIZE: The maximum aggregate cached bytes
+     *    - H_MEMPOOL_BIN_GROWTH: The growth factor. (Default: 2)
+     *    - H_MEMPOOL_MIN_BIN: The minimum bin. (Default: 1)
+     *    - H_MEMPOOL_MAX_BIN: The maximum bin. (Default: no max bin)
+     *    - H_MEMPOOL_MAX_CACHED_SIZE: The maximum aggregate cached bytes
      *      per device. (Default: No maximum)
-     *    - H_CUB_DEBUG: If nonzero, allow CUB to print debugging output.
+     *    - H_MEMPOOL_MAX_BIN_ALLOC_SIZE: The maximum cached bytes per
+     *      allocation. (Default: No maximum)
+     *    - H_MEMPOOL_BIN_MULT_THRESHOLD: The difference between two consecutive
+     *      geometric bins from which linear binning is used. (Default: No
+     *      linear binning)
+     *    - H_MEMPOOL_BIN_MULT: The multiplier for linear bin sizes. (Default:
+     *      No linear binning)
+     *    - H_MEMPOOL_BIN_SIZES: Custom set of (comma-separated) bin sizes in
+     *      bytes. (Default: None)
+     *    - H_MEMPOOL_MALLOCASYNC: If nonzero, uses mallocAsync as the backend
+     *      for non-binned allocation. (Default: 0)
+     *    - H_MEMPOOL_DEBUG: If nonzero, prints debugging output.
      *
      *  Note that if debugging output is turned on, there is no
      *  synchronization across processes. Users should take care to
      *  redirect output on a per-rank basis, either through the
      *  features exposed by their MPI launcher or by some other means.
      */
-    cub_impl::CachingDeviceAllocator& MemoryPool();
+    PooledDeviceAllocator& MemoryPool();
     /** Destroy singleton instance of CUB memory pool. */
     void DestroyMemoryPool();
 
